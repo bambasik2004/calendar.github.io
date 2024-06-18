ff095c62e2db6aab9607504301e76572585eccae
Bambasik2004 <bambasik2004@icloud.com>
2024-06-18 11:26:33 +0300

    v1

---
+++ main.py
@@ -0,0 +1,51 @@
+import asyncio
+import logging
+import sys
+from os import getenv
+from dotenv import load_dotenv
+
+from aiogram import Bot, Dispatcher, html
+from aiogram.client.default import DefaultBotProperties
+from aiogram.enums import ParseMode
+from aiogram.filters import CommandStart
+from aiogram.types import Message, WebAppData
+
+load_dotenv()
+TOKEN = getenv("BOT_TOKEN")
+
+# All handlers should be attached to the Router (or Dispatcher)
+dp = Dispatcher()
+
+
+@dp.message(CommandStart())
+async def command_start_handler(message: Message) -> None:
+    """
+    This handler receives messages with `/start` command
+    """
+    # Most event objects have aliases for API methods that can be called in events' context
+    # For example if you want to answer to incoming message you can use `message.answer(...)` alias
+    # and the target chat will be passed to :ref:`aiogram.methods.send_message.SendMessage`
+    # method automatically or call API method directly via
+    # Bot instance: `bot.send_message(chat_id=message.chat.id, ...)`
+    await message.answer(f"Hello, {html.bold(message.from_user.full_name)}!")
+
+@dp.message()
+async def get_data(message: Message):
+    if message.web_app_data:
+        web_app_data = message.web_app_data.data
+        await message.answer(f"Выбранная дата: {web_app_data}")
+    else:
+        await message.answer("Нет данных из веб-приложения.")
+
+async def main() -> None:
+    # Initialize Bot instance with default bot properties which will be passed to all API calls
+    bot = Bot(token=TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
+
+    # And the run events dispatching
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO, stream=sys.stdout)
+    asyncio.run(main())
+
